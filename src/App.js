957a676f106b923d797e0d5b3a52b32c89c02b11
maintainline <z.seon.dev@gmail.com>
2025-08-12 11:57:44 +0900

    [docs] animejs 기본 활용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import AddForm from "./components/editor/AddForm";
+import AniPage from "./pages/AniPage";
 
 function App() {
   return (
     <div>
-      <h1>Editor</h1>
-      <AddForm />
+      <h1>Anime 예제</h1>
+      <AniPage />
     </div>
   );
 }
